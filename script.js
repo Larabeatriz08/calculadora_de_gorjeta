696f91a5fb76af0dae46c59033ac6d07c7f77c8f
Lara Sousa Esteves <laradesouzalima08@gmail.com>
2024-03-25 17:54:50 -0300

    quarto commit js

--- script.js
+++ script.js
@@ -31,11 +31,11 @@ function receberQuantidadePessoas(evento) {
 
 const botoesGorjeta = document.querySelectorAll (".gorjeta input[type = 'button']")
 botoesGorjeta.forEach(botao => {
-    botao.addEventListener("click", receberPorcentagemBotao)
+    botao.addEventListener("click", receberPorcentagem)
 
 })
 
-function receberPorcentagemBotao (evento) {
+function receberPorcentagem (evento) {
 
       botoesGorjeta.forEach(botao => {
         botao.classList.remove("botao-ativo")
@@ -45,6 +45,15 @@ function receberPorcentagemBotao (evento) {
         }
       })
 
+      if(evento.target.value !== ""){
       porcentagem = parseFloat(evento.target.value) / 100
+    } else {
+        porcentagem = 0
+    }
+     
 
 }
+
+const gorjetaInput = document.querySelector("#outra")
+gorjetaInput.addEventListener("input", receberPorcentagem)
+
