58b6561e3f3a3f442ce1de49e2ba1fb97fbd3abf
Lara Sousa Esteves <laradesouzalima08@gmail.com>
2024-03-25 17:40:05 -0300

    segundo commit do js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let conta = 0
-
 let pessoas = 0
+let porcentagem = 0
 
 const contaInput = document.querySelector("#conta")
 contaInput.addEventListener("input", receberValorConta)
@@ -27,4 +27,14 @@ function receberQuantidadePessoas(evento) {
     pessoas = Number(evento.target.value)
 
    }
+}
+
+const botoesGorjeta = document.querySelectorAll (".gorjeta input[type = 'button']")
+botoesGorjeta.forEach(botao => {
+    botao.addEventListener("click", receberPorcentagem)
+
+})
+
+function receberPorcentagem (evento) {
+      porcentagem = Number(evento.target.value) 
 }
